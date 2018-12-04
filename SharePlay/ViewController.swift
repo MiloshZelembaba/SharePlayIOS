3c384f71f0ed34efb12afd7edae375de9c6af42a
Milosh Zelembaba <mzelemba@uwaterloo.ca>
2018-12-03 19:37:15 -0800

    Initial Commit

---
+++ SharePlay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SharePlay
+//
+//  Created by Milosh Zelembaba on 2018-12-03.
+//  Copyright © 2018 Milosh Zelembaba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
